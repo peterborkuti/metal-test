ca7409ac44b6f8e057a80ac8bccabd424f8c1592
Peter Borkuti <peter.borkuti@liferay.com>
2017-03-09 16:46:36 +0100

    select

--- src/Test01.js
+++ src/Test01.js
@@ -3,9 +3,109 @@
 import templates from './Test01.soy';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+import Tooltip from "metal-tooltip";
+import Modal from "metal-modal";
+import Toggler from "metal-toggler";
+import Autocomplete from "metal-autocomplete";
+import Select from "metal-select";
 
 class Test01 extends Component {
+
+    initTooltipExamples() {
+	new Tooltip({
+	    delay: [300, 150],
+	    elementClasses: 'fade',
+	    selector: '[data-title]',
+	    visible: false
+	});
+
+	new Tooltip({
+	    delay: [300, 150],
+	    elementClasses: 'fade',
+	    position: Tooltip.Align.Right,
+	    selector: '#tooltip1',
+	    title: 'tooltip 1',
+	    visible: false
+	});
+
+	console.log("tooltips initialized");
+
+    }
+
+    initModalExamples() {
+	var modal = new Modal({
+	    element: '.modal',
+	    header: '<h4 class="modal-title">Modal header</h4>',
+	    body: 'One fine body...',
+	    footer: '<button type="button" class="btn btn-primary closeModal">OK</button>',
+	    visible: false
+	});
+
+	var openElement = this.element.querySelector('.openModal');
+	openElement.addEventListener('click', function() {
+	    modal.show();
+	});
+
+	var closeElement = modal.element.querySelector('.closeModal');
+	closeElement.addEventListener('click', function() {
+	    modal.hide();
+	});
+
+	console.log("modals initialized");
+
+    }
+
+    initTogglerExamples() {
+	new Toggler({
+	    content: '.toggler-content',
+    	    header: '.btn-primary'
+	});
+	console.log("Toggler initialized");
+    }
+
+    initAutocompleteExample() {
+	var continents = ['Asia', 'Africa', 'North America', 'South America', 'Antarctica', 'Europe', 'Australia'];
+	new Autocomplete({
+	    element: this.element.querySelector('#autocompletelist'),
+	    inputElement: this.element.querySelector('#autocompleteinput'),
+	    data: function(query) {
+		    var arr = continents;
+		    var filteredArr = 
+			    arr.filter(function(item) {
+				return query && item.toLowerCase().indexOf(query.toLowerCase()) === 0;
+			    });
+		return filteredArr;
+	    }
+	});
+	console.log("Autocomplete initialized");
+    }
+
+    initSelectExample() {
+	new Select({
+	    label: '<i>Select</i>',
+	    items: ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
+	    hiddenFieldName: 'method'
+	}, this.element.querySelector('#first'));
+	new Select({
+	    items: ['Number', 'String', 'Boolean', 'Array', '<i>Object</i>'],
+	    values: ['Number', 'String', 'Boolean', 'Array', 'Object'],
+	    label: 'Type',
+	}, this.element.querySelector('#second'));
+
+	console.log("Select initialized");
+    }
+
+
+    rendered() {
+	this.initTooltipExamples();
+	this.initModalExamples();
+	this.initTogglerExamples();
+	this.initAutocompleteExample();
+	this.initSelectExample();
+    };
 }
+
+
 Soy.register(Test01, templates);
 
 export default Test01;
--- src/Test01.soy.js
+++ src/Test01.soy.js
@@ -43,7 +43,129 @@ var iattr = IncrementalDom.attr;
  */
 function $render(opt_data, opt_ignored, opt_ijData) {
   ie_open('div');
-    itext('Hello BP');
+    ie_open('div', null, null,
+        'id', 'tooltipexamples');
+      ie_open('h1');
+        itext('Tooltip');
+      ie_close('h1');
+      ie_open('div', null, null,
+          'class', 'row');
+        ie_open('div', null, null,
+            'id', 'tooltip1',
+            'class', 'col-md-4 text-right');
+          itext('Here should be a tooltip 1');
+        ie_close('div');
+        ie_open('div', null, null,
+            'data-title', 'tooltip 2',
+            'class', 'col-md-4 text-center');
+          itext('Here should be tooltip 2');
+        ie_close('div');
+        ie_open('div', null, null,
+            'data-title', 'tooltip 3',
+            'class', 'col-md-4 text-center');
+          itext('Here should be tooltip 3');
+        ie_close('div');
+      ie_close('div');
+    ie_close('div');
+    ie_open('hr');
+    ie_close('hr');
+    ie_open('div', null, null,
+        'id', 'modalexamples');
+      ie_open('h1');
+        itext('Modal');
+      ie_close('h1');
+      ie_void('div', null, null,
+          'class', 'modal');
+      ie_open('button', null, null,
+          'type', 'button',
+          'class', 'btn btn-primary openModal');
+        itext('Open Modal');
+      ie_close('button');
+    ie_close('div');
+    ie_open('hr');
+    ie_close('hr');
+    ie_open('div', null, null,
+        'id', 'togglerexamples');
+      ie_open('h1');
+        itext('Toggler');
+      ie_close('h1');
+      ie_open('div', null, null,
+          'class', 'row');
+        ie_open('div', null, null,
+            'class', 'col-md-6');
+          ie_open('button', null, null,
+              'class', 'btn btn-primary');
+            itext('Read more \u00BB');
+          ie_close('button');
+          ie_open('div', null, null,
+              'class', 'toggler-content toggler-collapsed');
+            itext('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea qui sint distinctio recusandae quo ut eaque provident eius eos commodi natus labore culpa modi soluta quia repudiandae nulla ex. Reiciendis cupiditate quis voluptatum atque veniam assumenda itaque perferendis eos voluptatem dolores aut eum. Ut voluptatum amet earum delectus totam unde!');
+          ie_close('div');
+        ie_close('div');
+        ie_open('div', null, null,
+            'class', 'col-md-6');
+          ie_open('button', null, null,
+              'class', 'btn btn-primary');
+            itext('Read more 2 \u00BB');
+          ie_close('button');
+          ie_open('div', null, null,
+              'class', 'toggler-content toggler-collapsed');
+            itext('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea qui sint distinctio recusandae quo ut eaque provident eius eos commodi natus labore culpa modi soluta quia repudiandae nulla ex. Reiciendis cupiditate quis voluptatum atque veniam assumenda itaque perferendis eos voluptatem dolores aut eum. Ut voluptatum amet earum delectus totam unde!');
+          ie_close('div');
+        ie_close('div');
+      ie_close('div');
+    ie_close('div');
+    ie_open('hr');
+    ie_close('hr');
+    ie_open('div', null, null,
+        'id', 'autocompleteexample');
+      ie_open('h1');
+        itext('Autocomplete');
+      ie_close('h1');
+      ie_open('p');
+        itext('Type in a continent\'s name (Africa, Asia, etc.)');
+      ie_close('p');
+      ie_open('div', null, null,
+          'class', 'row');
+        ie_open('div', null, null,
+            'class', 'col-md-6');
+          ie_open('input', null, null,
+              'id', 'autocompleteinput',
+              'type', 'text',
+              'class', 'form-control');
+          ie_close('input');
+        ie_close('div');
+        ie_open('div', null, null,
+            'class', 'col-md-6');
+          ie_void('div', null, null,
+              'id', 'autocompletelist');
+        ie_close('div');
+      ie_close('div');
+    ie_close('div');
+    ie_open('hr');
+    ie_close('hr');
+    ie_open('div', null, null,
+        'id', 'selectexample');
+      ie_open('h1');
+        itext('Select');
+      ie_close('h1');
+      ie_open('div', null, null,
+          'class', 'col-md-4');
+        ie_open('h1');
+          itext('Dropdown + input group left');
+        ie_close('h1');
+        ie_void('div', null, null,
+            'id', 'first');
+      ie_close('div');
+      ie_open('div', null, null,
+          'class', 'col-md-4');
+        ie_open('h1');
+          itext('Dropdown + label');
+        ie_close('h1');
+        ie_void('div', null, null,
+            'id', 'second');
+      ie_close('div');
+    ie_close('div');
   ie_close('div');
 }
 exports.render = $render;
